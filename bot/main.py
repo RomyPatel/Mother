b54b07f8d8dfb037b8afc54a48af76deeed84d3a
RomyPatel <romypatel392000@gmail.com>
2021-05-21 00:29:40 +0530

    Went Back

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,22 @@
 import discord
 import os
-import requests
-import json
-import random
+# import requests
+# import json
+# import random
 # from dotenv import load_dotenv
 
 # load_dotenv()
 
 client = discord.Client()
 
-sad_words= ["sad", "depressed", "unhappy", "miserable", "depressing"]
-basic_encouragements= ["Cheer up!", "You can do it!", "You are a good person!"]
+# sad_words= ["sad", "depressed", "unhappy", "miserable", "depressing"]
+# basic_encouragements= ["Cheer up!", "You can do it!", "You are a good person!"]
 
-def get_quote():
-    response= requests.get("https://zenquotes.io/api/random")
-    json_data= json.loads(response.text)
-    quote= json_data[0]['q'] + " -" + json_data[0]['a']
-    return quote
+# def get_quote():
+#     response= requests.get("https://zenquotes.io/api/random")
+#     json_data= json.loads(response.text)
+#     quote= json_data[0]['q'] + " -" + json_data[0]['a']
+#     return quote
 
 @client.event 
 async def on_ready():
@@ -26,13 +26,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    msg= message.content    
+    # msg= message.content    
 
-    if message.content.startswith('$inspire'):
-        quote= get_quote()
-        await message.channel.send(quote)
+    if message.content.startswith('$Hello'):
+        # quote= get_quote()
+        await message.channel.send("Hello!")
 
-    if any (word in msg for word in sad_words):
-        await message.channel.send(random.choice(basic_encouragements))
+    # if any (word in msg for word in sad_words):
+        # await message.channel.send(random.choice(basic_encouragements))
 
 client.run(os.getenv('TOKEN'))
